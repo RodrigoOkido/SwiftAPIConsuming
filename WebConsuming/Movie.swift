b59de20fd90e3356de045209cd771ca0e54b5a28
RodrigoOkido <rodrigookido@gmail.com>
2021-07-01 20:44:25 -0300

    Added API structure code.

---
+++ WebConsuming/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  WebConsuming
+//
+//  Created by Rodrigo Yukio Okido on 01/07/21.
+//
+
+import Foundation
+
+
+struct Movie {
+    var title: String
+    var description: String
+    var image: String = "https://image.tmdb.org/t/p/w500"
+    var rating_average: Double
+}
--- WebConsuming/ViewController.swift
+++ WebConsuming/ViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    let api_key: String = "3b3fe42086419ba7768f061008414e5b"
-    
     @IBOutlet weak var tableView: UITableView!
     
+    let api_key: String = "3b3fe42086419ba7768f061008414e5b"
+    var popular_movies: [Movie] = []
+    var now_playing: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let urlPopular: String = "https://api.themoviedb.org/3/movie/popular?api_key=\(api_key)"
+        let urlNowPlaying: String = "https://api.themoviedb.org/3/movie/now_playing?api_key=\(api_key)"
+
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
     
     
