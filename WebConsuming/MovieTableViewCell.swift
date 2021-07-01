1e1ea6709d6732281716c697be24e75ff5e7333e
RodrigoOkido <rodrigookido@gmail.com>
2021-07-01 17:13:29 -0300

    Base files

---
+++ WebConsuming/MovieTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieTableViewCell.swift
+//  WebConsuming
+//
+//  Created by Rodrigo Yukio Okido on 01/07/21.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDescription: UILabel!
+    @IBOutlet weak var movieRating: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- WebConsuming/ViewController.swift
+++ WebConsuming/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let api_key: String = "3b3fe42086419ba7768f061008414e5b"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
